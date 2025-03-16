15c5b7805fdff96c0a80681d5a627ae8aeb93db6
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-03-15 23:29:11 -0300

    launch function

--- eva/nerv.c
+++ eva/nerv.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-\
-void lauch_eva(){
-    printf("Lauching Eva!!!...");
-}
+
+    void launch_eva() {
+        int time = 10;
+        printf("Launching Eva!!!...\n");
+        for (int i = time - 1; i >= 0; i--) {
+            printf("Launching... [%d]\n", i);
+        }
+    }
 
 void EVA_00(){
 
@@ -19,5 +23,8 @@ void EVA_02(){
 }
 
 int main(int argc, char *argv[]){
+    lauch_eva();
+
+    return 0;
 
 }
