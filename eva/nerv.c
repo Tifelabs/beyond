919bbea9fc58e8ff526ec16865fcfba6c378b822
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-03-15 23:19:48 -0300

    evangelion

---
+++ eva/nerv.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+\
+void lauch_eva(){
+    printf("Lauching Eva!!!...");
+}
+
+void EVA_00(){
+
+}
+
+void EVA_01(){
+
+}
+
+void EVA_02(){
+
+}
+
+int main(int argc, char *argv[]){
+
+}
