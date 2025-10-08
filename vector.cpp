48f8cc32e688491bfdcb219fba03f0c299c69a39
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-10-08 02:48:38 -0300

    vectors quick learn

---
+++ vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define DASH "\t + + ======================= + +\n\n"
+
+int main(void){
+    std::vector<std::string> consoles = {"PlayStation", "Xbox", "Nintendo"};
+
+    for(std::string console : consoles){
+        std::cout << console << "\n";
+    }
+
+    std::cout<< "\n\n\n";
+    std::cout<< DASH;
+    
+    std::cout<< consoles.front() << "\n";
+    std::cout<< consoles.back() << "\n";
+
+    std::cout<< DASH;
+
+    consoles.push_back("Atari");
+    consoles.push_back("Sega");
+    for(std::string console : consoles){
+        std::cout << console << "\n";
+    }
+    
+
+
+    return 0;
+}
