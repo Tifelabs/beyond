05ba60933a05632d3ed7781e37c2faa581c6f165
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-12-05 00:12:46 -0700

    Best practice ffg

--- vec1.cpp
+++ vec1.cpp
@@ -2,15 +2,18 @@
 #include <vector>
 
 int main(int argc, char *argv[]){
-    std::vector<float> prices = {10.0, 90.05, 44.3};
-    const ElementSize = prices.size();
-    std::cout << "Size of Prices Before Modification\n:";
-    std::cout << ElementSize;
-
-    /* Adding Price to the Front */
-    prices.front() = 10;
-
+    std::vector<float> prices = {10.0f, 90.05f, 44.3f};
+    std::cout << "Size of Prices Before Modification: -> " << prices.size() << '\n';
+    
+    prices.push_back(4.75f);
+    prices.push_back(55.2f);
+    
+    std::cout << "ELEMENTS\n";
     for(const auto& x : prices){
-        std::cout << x << '\n';
+        std::cout << "$" << x << '\n';
     }
+    std::cout << "\nSize of Prices After Modification: -> " << prices.size() << '\n';
+
+    std::cin.get();
+    return 0;
 }
