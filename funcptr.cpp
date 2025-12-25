82af520a01f5dac265ef5cf727cd3327f269ce32
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-12-25 16:38:48 -0700

    Replace map with :: enum class

--- funcptr.cpp
+++ funcptr.cpp
@@ -15,18 +15,27 @@ typedef struct Account{
     int Age;
 } Account;
 
+enum class Continent{
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    ASIA,
+    AFRICA,
+    ANTARCTICA,
+    EUROPE,
+    AUSTRALIA,
+};
 
-std::map<std::string, std::string> Continents(){      
-    return{
-        {"NORTH AMERICA", "NA"},
-        {"SOUTH AMERICA", "SA"},
-        {"EUROPE", "EU"},
-        {"AFRICA", "AF"},
-        {"ASIA", "AS"},
-        {"AUSTRALIA", "AU"},
-        {"ANTARCTICA", "AN"}
-        };
-    }   
+std::string GetContinent(Continent Code){
+    switch(Code){
+        case Continent::NORTH_AMERICA: return "NA";
+        case Continent::SOUTH_AMERICA: return "SA";
+        case Continent::ASIA: return "AS";
+        case Continent::AFRICA: return "AF";
+        case Continent::ANTARCTICA: return "AN";
+        case Continent::EUROPE: return "EU";
+        case Continent::AUSTRALIA: return "AU";
+    };
+}
 
 int main(){
     
@@ -36,7 +45,7 @@ int main(){
     Account User = {"Zoe", "Tokyo", "AS", 22};
     std::cout << User.UserName << " is from " << User.City << '\n';
 
-    auto regions = Continents();
+    auto regions = Continent();
 
     std::cout << "Name: " << User.UserName << '\n';
     std::cout << "City: " << User.City << '\n';
