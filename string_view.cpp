33d6b41b7b95ec8f70d6f96268f2fccc5dc63d91
Tifelabs <boluwatifeolusegun018@gmail.com>
2026-03-05 23:24:33 -0700

    string_view :: remove(suffix && prefix)

--- string_view.cpp
+++ string_view.cpp
@@ -8,5 +8,14 @@ void printSV(std::string_view str){
 int main(){
     std::string_view s = "Testing String View\n";
     printSV(s);
+
+    std::string_view fruit {"Watermelon"};
+    std::cout << fruit << '\n';
+
+    fruit.remove_prefix(1);
+    std::cout << fruit << '\n';
+
+    fruit.remove_suffix(2);
+    std::cout << fruit << '\n';
     return 0;
 }
